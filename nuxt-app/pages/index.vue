be3fb087ea4f232af07a4c0131bd03b1d124f51e
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-01 17:38:31 +0330

    Create AboutUs component

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -2,7 +2,9 @@
   <Slider />
   <Features />
   <ProductTabs />
+  <AboutUs />
 </template>
 <script setup lang="ts">
 import Slider from "~/components/Slider.vue";
+import AboutUs from "~/components/AboutUs.vue";
 </script>
