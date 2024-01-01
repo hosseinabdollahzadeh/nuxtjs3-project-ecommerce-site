3518e9c92e9a6c62216e81f788384a6a98270e5e
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-01 16:20:32 +0330

    npm i bootstrap-icons, Add Header, Slider and Features components

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <LayoutHeader />
     <NuxtPage />
   </div>
 </template>
+<script setup lang="ts">
+import Slider from "~/components/Slider.vue";
+</script>
--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs:{
+        dir: 'rtl',
+        lang: 'fa'
+      }
+    }
+  },
   css: ["~/assets/css/main.css"],
 })
