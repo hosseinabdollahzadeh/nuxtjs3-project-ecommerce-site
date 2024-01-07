e76ad5e874ef69c7cb4fc77f050af910f400543c
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-07 16:23:22 +0330

    Add sort filter - delete page from route.query if there is no exist in param

--- nuxt-app/pages/menu.vue
+++ nuxt-app/pages/menu.vue
@@ -30,27 +30,31 @@
           <div>
             <label class="form-label">مرتب سازی</label>
             <div class="form-check my-2">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+              <input class="form-check-input" @click="handleFilter({sortBy: 'max'})" type="radio"
+                     name="flexRadioDefault" id="flexRadioDefault1">
               <label class="form-check-label cursor-pointer" for="flexRadioDefault1">
                 بیشترین قیمت
               </label>
             </div>
             <div class="form-check my-2">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
+              <input class="form-check-input" @click="handleFilter({sortBy: 'min'})" type="radio"
+                     name="flexRadioDefault" id="flexRadioDefault2"
                      checked>
               <label class="form-check-label cursor-pointer" for="flexRadioDefault2">
                 کمترین قیمت
               </label>
             </div>
             <div class="form-check my-2">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3"
+              <input class="form-check-input" @click="handleFilter({sortBy: 'bestseller'})" type="radio"
+                     name="flexRadioDefault" id="flexRadioDefault3"
                      checked>
               <label class="form-check-label cursor-pointer" for="flexRadioDefault3">
                 پرفروش ترین
               </label>
             </div>
             <div class="form-check my-2">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4"
+              <input class="form-check-input" @click="handleFilter({sortBy: 'sale'})" type="radio"
+                     name="flexRadioDefault" id="flexRadioDefault4"
                      checked>
               <label class="form-check-label cursor-pointer" for="flexRadioDefault4">
                 با تخفیف
@@ -110,6 +114,9 @@ const {data, refresh, pending} = await useFetch(() => `${apiBaseUrl}/menu`, {
 function handleFilter(param) {
   query.value = {...route.query, ...param}
 
+  if (!param.hasOwnProperty('page')) {
+    delete query.value.page;
+  }
   router.push({
     path: '/menu',
     query: query.value
