322e96fdef89ada4c7a8030313deec337e810d55
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-07 14:43:33 +0330

    Fetch and show products on menu page

--- nuxt-app/pages/menu.vue
+++ nuxt-app/pages/menu.vue
@@ -58,35 +58,8 @@
         </div>
         <div class="col-sm-12 col-lg-9">
           <div class="row gx-3">
-            <div class="col-sm-6 col-lg-4">
-              <div class="box">
-                <div>
-                  <div class="img-box">
-                    <img src="./images/b1.jpg" alt="">
-                  </div>
-                  <div class="detail-box">
-                    <h5>
-                      لورم ایپسوم متن
-                    </h5>
-                    <p>
-                      لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
-                      از
-                      طراحان
-                      گرافیک است.
-                    </p>
-                    <div class="options">
-                      <h6>
-                        <del>45,000</del>
-                        34,000
-                        <span>تومان</span>
-                      </h6>
-                      <a href="">
-                        <i class="bi bi-cart-fill text-white fs-5"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div v-for="product in data.data.products" :key="product.id" class="col-sm-6 col-lg-4">
+              <Card :product="product" />
             </div>
 
           </div>
@@ -104,6 +77,9 @@
 </template>
 
 <script setup>
+import Card from "~/components/product/Card.vue";
+
 const {public: {apiBaseUrl}} = useRuntimeConfig();
-const {data: categories, error} = await useFetch(`${apiBaseUrl}/categories`);
+const {data: categories} = await useFetch(`${apiBaseUrl}/categories`);
+const {data} = await useFetch(`${apiBaseUrl}/menu`);
 </script>
