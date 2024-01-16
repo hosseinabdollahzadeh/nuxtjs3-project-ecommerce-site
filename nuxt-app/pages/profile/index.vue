c7fd6aa886c3f15d670829dc200a2ca66fe923ed
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-16 17:25:19 +0330

    Update user profile info

--- nuxt-app/pages/profile/index.vue
+++ nuxt-app/pages/profile/index.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="vh-70">
+    <div v-if="errors.length > 0" class="alert alert-danger">
+      <ul class="mb-0">
+        <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+      </ul>
+    </div>
     <FormKit type="form" @submit="edit" :incomplete-message="false" :actions="false">
       <div class="row g-4">
         <div class="col col-md-6">
@@ -22,6 +27,7 @@
 
         <FormKit type="submit" input-class="btn btn-primary mt-4">
           ویرایش
+          <div v-if="loading" class="spinner-border spinner-border-sm sm-2"></div>
         </FormKit>
       </div>
     </FormKit>
@@ -29,12 +35,29 @@
 </template>
 
 <script setup>
+import { useToast } from "vue-toastification";
+const errors = ref([]);
+const loading = ref(false);
+const toast = useToast();
 
 const {data: user} = await useFetch('/api/profile/info', {
   headers: useRequestHeaders(['cookie'])
 });
 
-function edit(formData) {
-  console.log(formData)
+async function edit(formData) {
+  try {
+    loading.value = true;
+    errors.value = [];
+
+    await $fetch('/api/profile/info/edit', {
+      method: 'POST',
+      body: formData
+    })
+    toast.success("ویرایش اطلاعات شما با موفقیت انجام شد.");
+  } catch (error) {
+    errors.value = Object.values(error.data.data.message).flat();
+  } finally {
+    loading.value = false;
+  }
 }
 </script>
