132a4aa2131d720e9009c27023014d546c94df6b
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-21 10:02:09 +0330

    Show cartItems on cart page

--- nuxt-app/store/cart.js
+++ nuxt-app/store/cart.js
@@ -1,29 +1,51 @@
 import {defineStore} from 'pinia';
-import { useToast } from "vue-toastification"
+import {useToast} from "vue-toastification"
 
 const toast = useToast();
 export const useCartStore = defineStore('cart', {
     state: () => {
-        return{
-            cart : []
+        return {
+            cart: []
         }
     },
-    getters:{
-        count(state){
+    getters: {
+        count(state) {
             return state.cart.length
+        },
+        allItems(state) {
+            return state.cart
         }
     },
-    actions:{
-        addToCart(product, count){
+    actions: {
+        addToCart(product, count) {
             this.cart.push({
                 ...product,
                 qty: count
             })
-            // console.log(this.cart, 'ccart')
             toast.success('محصول به سبد خرید اضافه شد.')
         },
-        remove(id){
+        increment(id) {
+            const item = this.cart.find(p => p.id == id);
+            if (item) {
+                if (item.qty < item.quantity) {
+                    item.qty++
+                }
+            }
+        },
+        decrement(id) {
+            const item = this.cart.find(p => p.id == id);
+            if (item) {
+                if (item.qty > 1) {
+                    item.qty++
+                }
+                item.qty--
+            }
+        },
+        remove(id) {
             this.cart = this.cart.filter(p => p.id != id)
+        },
+        clear() {
+            this.cart = [];
         }
     },
     persist: {
