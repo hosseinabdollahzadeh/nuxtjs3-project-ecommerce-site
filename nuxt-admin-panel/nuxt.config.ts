38fab7ab39294440ee2bac168ea98101824de221
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-25 18:19:19 +0330

    Add fonts and main.css and bootstrap and bootstrap-icons css files

--- nuxt-admin-panel/nuxt.config.ts
+++ nuxt-admin-panel/nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+
+    app: {
+        head: {
+            htmlAttrs: {
+                dir: 'rtl',
+                lang: 'fa'
+            }
+        }
+    },
+
+    runtimeConfig: {
+        public: {
+            apiBaseUrl: "http://laravel9-api-project-ecommerce.test/api/admin-panel"
+        }
+    },
+
+    css: ["~/assets/css/main.css"],
+
+    // for old browser
+    build: {
+        transpile: ['vue-toastification'],
+    }
 })
