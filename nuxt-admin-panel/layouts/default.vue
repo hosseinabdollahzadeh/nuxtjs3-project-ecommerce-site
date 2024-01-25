f83a6b97c6e0478260dfc4e861fad997cc9c22ef
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-25 18:34:16 +0330

    Add layout

---
+++ nuxt-admin-panel/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <LayoutHeader />
+
+  <div class="container-fluid">
+    <div class="row">
+      <LayoutSidebar />
+
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
