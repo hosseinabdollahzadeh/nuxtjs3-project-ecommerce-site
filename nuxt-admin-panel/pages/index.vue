b2b626236106724b6d7745d4f8f52c294151d881
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-30 10:04:58 +0330

    Create global endpoint - get chartData on admin panel index

--- nuxt-admin-panel/pages/index.vue
+++ nuxt-admin-panel/pages/index.vue
@@ -1,9 +1,16 @@
 <template>
-Index Page
+  Index Page
 </template>
 
 <script setup>
 definePageMeta({
   middleware: 'auth'
 })
+
+const {data: chartData} = await useFetch('/api/global', {
+  query: {url: '/transactions/chart'},
+  headers: useRequestHeaders(['cookie'])
+})
+
+console.log(chartData.value)
 </script>
