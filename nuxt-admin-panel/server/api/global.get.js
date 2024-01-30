fc87ca611f97babd95c42a47729dba5f9705d957
Hossein <abdollahzadeh.hossein@gmail.com>
2024-01-30 13:20:56 +0330

    Get and show user list on admin panel

--- nuxt-admin-panel/server/api/global.get.js
+++ nuxt-admin-panel/server/api/global.get.js
@@ -2,8 +2,11 @@ export default defineEventHandler(async (event) => {
     const {public: {apiBaseUrl}} = useRuntimeConfig();
     const token = getCookie(event, 'token');
     const query = getQuery(event);
+
+    let page = query.hasOwnProperty('page') ? `?page=${query.page}` : '';
+
     try {
-        const data = await $fetch(`${apiBaseUrl}${query.url}`, {
+        const data = await $fetch(`${apiBaseUrl}${query.url}${page}`, {
             headers: {
                 'Accept' : 'application/json',
                 'Authorization': `Bearer ${token}`
