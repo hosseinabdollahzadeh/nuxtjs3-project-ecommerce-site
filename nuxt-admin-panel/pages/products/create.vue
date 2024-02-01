87a6645fed89bf4bca4ea785d3fabe982c6e155d
Hossein <abdollahzadeh.hossein@gmail.com>
2024-02-01 15:05:41 +0330

    Create product form - Vue3PersianDatetimePicker

--- nuxt-admin-panel/pages/products/create.vue
+++ nuxt-admin-panel/pages/products/create.vue
@@ -63,6 +63,41 @@
                    message-class="form-text text-danger"/>
         </div>
 
+        <div class="col-md-3">
+          <label class="form-label">تاریخ شروع حراجی</label>
+          <div class="input-group mb-3">
+            <span class="input-group-text"><i class="bi bi-clock"></i></span>
+            <input type="text" class="form-control" id="sale-date-from-picker"/>
+          </div>
+          <date-picker v-model="saleDateFrom" type="datetime" display-format="HH:mm jYYYY-jMM-jDD"
+                       format="YYYY-MM-DD HH:mm:ss"
+                       custom-input="#sale-date-from-picker"/>
+        </div>
+
+        <div class="col-md-3">
+          <label class="form-label">تاریخ پایان حراجی</label>
+          <div class="input-group mb-3">
+            <span class="input-group-text"><i class="bi bi-clock"></i></span>
+            <input type="text" class="form-control" id="sale-date-to-picker"/>
+          </div>
+          <date-picker v-model="saleDateTo" type="datetime" display-format="HH:mm jYYYY-jMM-jDD"
+                       format="YYYY-MM-DD HH:mm:ss"
+                       custom-input="#sale-date-to-picker"/>
+        </div>
+
+        <div class="col-md-3">
+          <label for="images" class="form-label">تصاویر</label>
+          <input @change="imagesFile" type="file" class="form-control" id="images" multiple/>
+        </div>
+
+        <div class="col-md-12">
+          <formKit type="textarea" rows="5" name="description" id="description" label="توضیحات"
+                   label-class="form-label" input-class="form-control" message-class="form-text text-danger"
+                   validation="required"
+                   :validation-messages="{required: 'فیلد توضیحات الزامی است.'}"/>
+        </div>
+
+
         <FormKit type="submit" input-class="btn btn-sm btn-outline-dark mt-3 mb-5">
           ایجاد محصول
           <!--      <div v-if="loading" class="spinner-border spinner-border-sm sm-2"></div>-->
@@ -77,18 +112,22 @@
 <script setup>
 
 const primaryImage = ref(null)
+const saleDateFrom = ref(null)
+const saleDateTo = ref(null)
+const images = ref(null)
 
 const {data: categories} = await useFetch('/api/global', {
   query: {url: '/categories'},
   headers: useRequestHeaders(['cookie'])
 })
 
-
-console.log(categories.value)
+function imagesFile(el) {
+  images.value = el.target.files
+}
 
 function create(formData) {
 
-  console.log(formData)
+  console.log(saleDateFrom.value, saleDateTo.value)
 }
 
 </script>
