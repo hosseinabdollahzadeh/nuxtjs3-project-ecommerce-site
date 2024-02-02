78e7d87272c0387e0e014e52c1772fa984b55628
Hossein <abdollahzadeh.hossein@gmail.com>
2024-02-02 13:37:08 +0330

    Create and edit categories on admin panel

---
+++ nuxt-admin-panel/pages/categories/create.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h4 class="fw-bold">ایجاد دسته بندی</h4>
+  </div>
+
+  <div v-if="errors.length > 0" class="alert alert-danger col-md-3 m-auto mb-4">
+    <ul class="mb-0">
+      <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+    </ul>
+  </div>
+
+  <FormKit type="form" id="createAddress" @submit="create" :incomplete-message="false" :actions="false">
+    <div class="row g-4">
+      <div class="col col-md-3">
+        <FormKit type="text" name="name" id="name" label="نام" label-class="form-label"
+                 input-class="form-control" validation="required"
+                 :validation-messages="{required: 'فیلد نام الزامی است.'}"
+                 message-class="form-text text-danger"/>
+      </div>
+      <div class="col col-md-3">
+        <FormKit type="text" name="description" id="description" label="توضیحات" label-class="form-label"
+                 input-class="form-control"/>
+      </div>
+    </div>
+    <div>
+      <FormKit type="submit" input-class="btn btn-sm btn-outline-dark mt-3">
+        ایجاد دسته بندی
+        <div v-if="loading" class="spinner-border spinner-border-sm sm-2"></div>
+      </FormKit>
+    </div>
+  </FormKit>
+</template>
+<script setup>
+import {useToast} from "vue-toastification";
+
+const errors = ref([]);
+const loading = ref(false);
+const toast = useToast();
+
+async function create(formData) {
+
+  try {
+    loading.value = true;
+    errors.value = [];
+
+    await $fetch('/api/global', {
+      method: 'POST',
+      body: formData,
+      query: {url: '/categories'}
+    })
+    toast.success("ایجاد دسته بندی با موفقیت انجام شد.");
+    return navigateTo('/categories')
+  } catch (error) {
+    errors.value = Object.values(error.data.data.message).flat();
+  } finally {
+    loading.value = false;
+  }
+}
+</script>
--- nuxt-admin-panel/pages/categories/edit/[id].vue
+++ nuxt-admin-panel/pages/categories/edit/[id].vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h4 class="fw-bold">ویرایش کاربر</h4>
+    <h4 class="fw-bold">ویرایش دسته بندی</h4>
   </div>
 
   <div v-if="errors.length > 0" class="alert alert-danger col-md-3 m-auto mb-4">
@@ -13,33 +13,20 @@
     <div class="row g-4">
       <div class="col col-md-3">
         <FormKit type="text" name="name" id="name" label="نام" label-class="form-label"
-                 :value="user.name"
+                 :value="category.name"
                  input-class="form-control" validation="required"
                  :validation-messages="{required: 'فیلد نام الزامی است.'}"
                  message-class="form-text text-danger"/>
       </div>
       <div class="col col-md-3">
-        <FormKit type="email" name="email" id="email" label="ایمیل" label-class="form-label"
-                 :value="user.email"
-                 input-class="form-control" validation="required"
-                 :validation-messages="{required: 'فیلد ایمیل الزامی است.'}"
-                 message-class="form-text text-danger"/>
-      </div>
-      <div class="col col-md-3">
-        <FormKit type="text" name="cellphone" id="cellphone" label="شماره تماس" label-class="form-label"
-                 :value="user.cellphone"
-                 input-class="form-control" :validation="[['required'], ['matches', /^(\+98|0)?9\d{9}$/]]"
-                 :validation-messages="{required:'فیلد شماره تماس الزامی است.', matches:'شماره تماس معتبر نمی باشد.'}"
-                 message-class="form-text text-danger"/>
-      </div>
-      <div class="col col-md-3">
-        <FormKit type="password" name="password" id="password" label="رمز عبور" label-class="form-label"
-                 input-class="form-control" message-class="form-text text-danger"/>
+        <FormKit type="text" name="description" id="description" label="توضیحات" label-class="form-label"
+                 :value="category.description"
+                 input-class="form-control" />
       </div>
     </div>
     <div>
       <FormKit type="submit" input-class="btn btn-sm btn-outline-dark mt-3">
-        ویرایش کاربر
+        ویرایش دسته بندی
         <div v-if="loading" class="spinner-border spinner-border-sm sm-2"></div>
       </FormKit>
     </div>
@@ -54,8 +41,8 @@ const toast = useToast();
 
 const route = useRoute()
 
-const {data: user} = await useFetch('/api/global', {
-  query: {url: `/users/${route.params.id}`},
+const {data: category} = await useFetch('/api/global', {
+  query: {url: `/categories/${route.params.id}`},
   headers: useRequestHeaders(['cookie'])
 })
 
@@ -68,10 +55,10 @@ async function update(formData) {
     await $fetch('/api/global', {
       method: 'PUT',
       body: formData,
-      query: {url: `/users/${user.value.id}`}
+      query: {url: `/categories/${category.value.id}`}
     })
-    toast.success("ویرایش کاربر با موفقیت انجام شد.");
-    return navigateTo('/users')
+    toast.success("ویرایش دسته بندی با موفقیت انجام شد.");
+    return navigateTo('/categories')
   } catch (error) {
     errors.value = Object.values(error.data.data.message).flat();
   } finally {
